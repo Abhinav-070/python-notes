c5ad1cf09e2b0ea0dafdb39b7ffecdb5f3d5d6c5
Abhinav A <abhinav59824@gmail.com>
2026-08-06 11:40:32 +0530

    notes

---
+++ file_handling.py
@@ -0,0 +1,31 @@
+# f=open("sample.txt","w")      # creates a file where the python file exits
+
+# f=open(r"C:\Users\MYPC\Desktop\Abhinav\python\sample.txt","w") 
+# f.write("Hello World")
+# f=open("sample.txt","r") 
+# print(f.read())
+# print(f.readline())     # reads line by line
+# print(f.readlines())    # reads all lines     
+# f=open("sample.txt","a") 
+# f.write("Learn C\n")
+# f.close()
+#print(f.read())
+
+# f=open("sample1.txt","x")    #creates a new file,if exists it will show error
+# f=open("image.jpeg","rb")
+# print(f.read(20))
+# f.close()
+
+# p=open("sample.txt","r+")
+# print(p.read())
+# p.write("goodbye\n")
+# print(p.read())
+# p.close()
+
+# p=open("sample3.txt","a+")  # write and read,but existing content will be deleted.if doesn't exist it will create new 
+# print(p.read())
+# p.write("goodbye!\n")
+# print(p.read())
+# p.close()
+
+
