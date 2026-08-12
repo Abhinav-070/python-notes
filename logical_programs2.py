703fac28ff151f5f4948c25708ff875f6db72577
Abhinav A <abhinav59824@gmail.com>
2026-08-12 12:30:55 +0530

    lp-2

---
+++ logical_programs2.py
@@ -0,0 +1,77 @@
+# 1
+# ch = input("Enter a Character:")
+# print(ord(ch))
+
+# 2
+# ch = input("Enter a Character:")
+# print(ord(ch))
+
+# 3
+# num = int(input("Enter a number:"))
+# print(chr(num))
+
+#4
+# word = "text"
+# for ch in word:
+#     print(ch,":",ord(ch))
+
+#5
+# num = int(input("Enter a number:"))
+# print(chr(num))
+
+#6
+# ch = input("Enter a character:")
+# if ord(ch)>= 65 and ord(ch)<=90:
+#     print("Uppercase")
+# else:
+#     print("Not Uppercase")
+
+#7
+# ch = input("Enter a character:")
+# if ord(ch) >=91 and ord(ch) <=122:
+#     print("Lowercaase")
+# else:
+#     print("Not Lowercase")
+
+#8
+# ch = input("Enter a character:")
+# if ord(ch) >= 48 and ord(ch) <= 57:
+#     print("The character is a digit")
+# else:
+#     print("The character is not a digit")
+
+#9
+# word ="teXt"
+# cnt= 0
+# for ch in word:
+#     if ord(ch)>= 65 and ord(ch)<=90:
+#         cnt = cnt+1
+# print(cnt)
+
+#10
+# word="l1iab2"
+# cnt = 0
+# for ch in word:
+#     if ord(ch) >= 48 and ord(ch) <= 57:
+#         cnt = cnt+1
+# print(cnt)
+
+#11
+# text = input("Enter the character:")
+# sep=["-",";",":"]
+# for ch in text:
+#     if ch in sep:
+#         print("Seperator")
+#     else:
+#         print("No Seperator")
+
+#12
+# text = input("Enter the character:")
+# sep=["-",";",":"]
+# cnt = 0
+# for ch in text:
+#     if ch in sep:
+#         cnt = cnt+1
+# print(cnt)
+
+#13
