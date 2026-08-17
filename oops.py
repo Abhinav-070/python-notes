14f8cc37498e8a0a4a7f1c877391cf280d97a6c0
Abhinav A <abhinav59824@gmail.com>
2026-08-17 13:15:23 +0530

    oops

---
+++ oops.py
@@ -0,0 +1,69 @@
+# class Dog:
+#     # pass
+#     name = "rex"
+#     age = 2
+#     def bark(self):
+#         print("the dog is barking")
+# d=Dog()
+# # print(d.name)
+# # print(d.age)
+# d.bark()
+
+# class Dog:
+#     # name="Bobby"
+#     def __init__(self, name):
+#         self.name = name
+#     def bark(self):
+#         print(f"{self.name} is barking")
+
+# d=Dog("bobby")
+# print(d)
+# d.bark()
+# d = Dog("tommy")
+# print(d)
+# d.bark()
+# d1=Dog("max")
+# print(d1)
+# d1.bark()
+
+
+# class Car:
+#     def drive(self):
+#         print("Car is moving")
+# c = Car()
+# c.drive()
+
+# class Student:
+#     name = "Rahul"
+#     def stud(self):
+#         print(f"{self.name} is studying")
+# d= Student()
+# d.stud()
+
+# class Student:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+# s = Student("Anu",10)
+# print(s.name)
+# print(s.age)
+
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def display(self):
+#         print(self.brand,self.model)
+# c = Car("Toyota","Innova")
+# c.display()
+
+class Students:
+    def __init__(self,name):
+        self.name=name
+    def introduce (self):
+        print("My name is ",self.name)
+
+s1= Students("Anu")
+s1.introduce()
+s2= Students("Rahul")
+s2.introduce()
